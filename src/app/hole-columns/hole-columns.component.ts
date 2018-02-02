382af221687c891d95e7e0b3bc69227bee38ac29
Steve Thompson <writetome51@gmail.com>
2018-02-02 13:57:01 -0700

    more changes to hole-columns html template

--- src/app/hole-columns/hole-columns.component.ts
+++ src/app/hole-columns/hole-columns.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {isNumber} from 'util';
 
 @Component({
   selector: 'hole-columns',
@@ -6,11 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HoleColumnsComponent implements OnInit {
 
-    columnIDs = [];
+    columnIDs = [
+        '1', '2', '3', '4',
+        '5', '6', '7', '8',
+        '9', 'out', '10', '11',
+        '12', '13', '14', '15',
+        '16', '17', '18', 'in', 'total'
+    ];
+
+    nonPlayerRows = [
+        'yards', 'par', 'hcp'
+    ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getColumnIDForHTML(id){
+      let prefix = 'hole-';
+      let suffix = '-column';
+      if (isNaN(id)){ return id + suffix; }
+      else{
+          return (prefix + id + suffix);
+      }
+  }
+
+
+
+  isTotalColumn(id) {
+      return (id === 'out' || id === 'in' || id === 'total');
   }
 
 }
