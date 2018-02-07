066d61685c3c0c70d4d8cf14b8eef0360894a2c0
Steve Thompson <writetome51@gmail.com>
2018-02-07 15:29:15 -0700

    new commit

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -4,43 +4,52 @@ import {GameService} from '../../services/game.service';
 import {PlayerNumbersService} from '../../services/player-numbers.service';
 
 
-
 @Component({
     selector: 'welcome',
     templateUrl: 'welcome.component.html'
 })
 
-export class WelcomeComponent implements OnInit{
+export class WelcomeComponent implements OnInit {
 
     email = '';
     password = '';
     playerNumbers: string[];
     gameIDs: string[] = [];
+    chosenGame: string;
 
 
     constructor(private _angularFireAuth: AngularFireAuth,
                 private _game: GameService,
                 playerNumbersService: PlayerNumbersService) {
+
         this.playerNumbers = playerNumbersService.self;
+
     }
 
 
     ngOnInit() {
-       this._setGameIDs();
+        this._setGameIDs();
+        this.chosenGame = this.gameIDs[this.gameIDs.length - 1];
     }
 
 
-    getPlayerPlaceholder(){
+    getPlayerPlaceholder() {
         // Find out how many players are already signed up to the game.
     }
 
 
-    private _setGameIDs(){
+    setGame() {
+        console.log(this.chosenGame);
+    }
+
+
+    private _setGameIDs() {
         this._game.getAll((games) => {
             games.forEach((game) => {
                 this.gameIDs.push(game.payload.doc.id);
             });
         });
     }
+
 
 }
